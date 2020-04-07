d11892c9367109aaf9bf32ed495901d13e237081
Erik Brozek <erik@brozek.name>
2020-04-07 15:40:40 +0200

    Changed: python mysqldb -> pymysql

--- mysqlwrapper/mysqlwrapper.py
+++ mysqlwrapper/mysqlwrapper.py
@@ -17,7 +17,8 @@ from logni import log
 
 
 try:
-	import MySQLdb
+	#import MySQLdb
+	import pymysql
 except ImportError as import_err:
 	log.error('run $ pip install -r requirements.txt, please [err="%s"]', import_err, 2)
 	#sys.exit(1)
@@ -48,17 +49,19 @@ class Connect(object):
 			'port':param.get('port', 3306),\
 			'connect_timeout':param.get('connect_timeout', 5),\
 			'dict_cursor':param.get('dict_cursor', 1),\
+			'dict_cursor':param.get('dict_cursor', 1),\
 			'charset':param.get('charset', 'utf8'),\
 			'autocommit':param.get('autocommit', 1),\
 			'dummy':param.get('dummy', 1),\
 			'debug':param.get('debug', 0),\
+			'separate connect':param.get('separate connect', 0),\
 			'name':'',\
 		}
 
 		# connect
 		self.__thread_id = 0
 		self.__connect_time = 0
-		self.__dbh = None
+		self.__dbc = None
 		self.connect()
 
 
@@ -73,17 +76,19 @@ class Connect(object):
 		self.__name = self.__param['name']
 
 		# dbh
-		self.__dbh = None
+		self.__dbc = None
 
 		# connect
 		try:
-			self.__dbh = MySQLdb.Connect(\
-				host=self.__param['host'],\
-				db=self.__param['db'],\
-				user=self.__param['user'],\
-				passwd=self.__param['passwd'],\
+
+			dbc = pymysql.connect(host=self.__param['host'],
+				user=self.__param['user'],
+				password=self.__param['passwd'],
+				db=self.__param['db'],
 				port=self.__param['port'],\
-				connect_timeout=self.__param['connect_timeout'])
+				charset=self.__param['charset'],
+				cursorclass=pymysql.cursors.DictCursor)
+
 			self.__connect_time = time.time() - start_time
 
 		except BaseException as emsg:
@@ -99,24 +104,49 @@ class Connect(object):
 			raise
 
 		# set autocommit an charset
-		self.__dbh.autocommit(self.__param['autocommit'])
-		self.__dbh.set_character_set(self.__param['charset'])
+		dbc.autocommit(self.__param['autocommit'])
+
+		self.__dbc = dbc
 
 		# connect time
 		self.__connect_time = int((time.time() - start_time) * 1000)
 
 		log.info('mysql INIT host=%s:%s, user=%s -> name=%s, thread_id=%s, time=%sms, connect OK',\
 			(self.__param['host'], self.__param['port'], self.__name,\
-			self.__param['user'], self.__dbh.thread_id(), self.__connect_time), priority=2)
+			self.__param['user'], self.thread_id(), self.__connect_time), priority=2)
 
+		# set wait_timeout
+		cursor = dbc.cursor()
+		cursor.execute('SET wait_timeout = 3600')
+		cursor.close()
+
+		return dbc
+
+
+	def __dbi(self):
+
+		if self.__param['separate connect'] == 1:
+			return self.connect()
+
+		return self.__dbc
+
+
+	def __db_close(self, dbh):
+
+		
+		if self.__param['separate connect'] == 1:
+			log.debug('mysql %s close -> thread_id=%s', (self.__name, dbh.thread_id), priority=2)
+			dbh.close()
+			return True
 
 		return True
+		
 
 
 	def thread_id(self):
 		""" thread id """
 
-		self.__thread_id = self.__dbh.thread_id()
+		self.__thread_id = self.__dbc.thread_id()
 		return self.__thread_id
 
 
@@ -124,10 +154,10 @@ class Connect(object):
 		""" commit """
 
 		try:
-			self.__dbh.commit()
+			self.__dbc.commit()
 		except BaseException as emsg:
 			log.error('mysql %s commit: err="%s"',\
-				(self._name, emsg), priority=2)
+				(self.__name, emsg), priority=2)
 			return False
 
 		log.info('mysql %s commit', self.__name, priority=2)
@@ -137,14 +167,16 @@ class Connect(object):
 	def close(self):
 		""" close """
 
+		thread_id = self.__dbc.thread_id()
+
 		try:
-			self.__dbh.close()
+			self.__dbc.close()
 		except BaseException as emsg:
-			log.error('mysql %s close: err="%s"',\
-				(self.__name, emsg), priority=2)
+			log.error('mysql %s close: thread_id=%s, err="%s"',\
+				(self.__name, thread_id, emsg), priority=2)
 			return False
 
-		log.info('mysql %s close', self.__name, priority=2)
+		log.info('mysql %s close: thread_id=%s', (self.__name, thread_id), priority=2)
 		return True
 
 
@@ -173,7 +205,7 @@ class Connect(object):
 		#	
 		#	# reset cursor + try reconnect
 		#	cursor.close()
-		#	cursor = self.__dbh.cursor(MySQLdb.cursors.DictCursor)
+		#	cursor = self.__dbc.cursor()
 		#
 		#	found = cursor.execute(query, param)
 		#
@@ -185,10 +217,10 @@ class Connect(object):
 
 		run_time = int((time.time() - start_time) * 1000)
 
-		log.info('mysql %s execute [%s] query: "%s" time=%sms found=%s dbh.thread_id=%s',\
+		log.info('mysql %s execute [%s] query: "%s" time=%sms found=%s dbc.thread_id=%s',\
 			(self.__name, rand, __query, run_time, found, self.__thread_id), priority=2)
 
-		return found
+		return cursor, found
 
 
 	def __fetchall(self, cursor, rand=0):
@@ -196,49 +228,37 @@ class Connect(object):
 
 		start_time = time.time()
 
-		ret = cursor.fetchall()
+		data = cursor.fetchall()
 
 		run_time = int((time.time() - start_time) * 1000)
 
 		log.info('mysql %s fetchall [%s] query: time=%sms found=%s',\
-			(self.__name, rand, run_time, len(ret)), priority=2)
+			(self.__name, rand, run_time, len(data)), priority=2)
 
-		return ret
+		return cursor, data
 
 
 	def query(self, query, param=()):
 		""" sql query """
 
 		rand = random.randint(1000, 9999)
-		cursor = self.__dbh.cursor(MySQLdb.cursors.DictCursor)
-		found = self.__execute(cursor, query, param, rand)
+		dbh = self.__dbi()
+		cursor = dbh.cursor()
+		cursor, found = self.__execute(cursor, query, param, rand)
 
 		if found == 0:
 			cursor.close()
-			del cursor
+			self.__db_close(dbh)
+			del rand, cursor, dbh
 			return found, []
 
-		data = self.__fetchall(cursor, rand)
+		cursor, data = self.__fetchall(cursor, rand)
 		cursor.close()
+		self.__db_close(dbh)
 		
-		del rand, cursor
+		del rand, cursor, dbh
 		return found, data
 
-
-	def insert_id(self):
-		""" last insert_id """
-
-		try:
-			insert_id = self.__dbh.insert_id()
-		except BaseException as base_err:
-			log.error('mysql %s insert_id(): err=%s',\
-				(self.__name, base_err), priority=2)
-			return 0
-
-		log.info('mysql %s insert_id(): %s',\
-			(self.__name, insert_id), priority=2)
-
-		return insert_id
 
 
 
@@ -263,8 +283,9 @@ class Connect(object):
 
 		rand = random.randint(1000, 9999)
 
-		cursor = self.__dbh.cursor(MySQLdb.cursors.DictCursor)
-		found = self.__execute(cursor, 'SELECT %s FROM `%s` %s %s' % (\
+		dbh = self.__dbi()
+		cursor = dbh.cursor()
+		cursor, found = self.__execute(cursor, 'SELECT %s FROM `%s` %s %s' % (\
 			_sql_column(column_list, table_name), table_name,\
 			sql_where, _sql_limit(limit)),\
 			sql_param, rand)
@@ -273,25 +294,31 @@ class Connect(object):
 			#self.__cursor.nextset()
 			#log.debug('mysql %s nextset -> found=0', self.__name)
 			cursor.close()
-			del rand, cursor
+			self.__db_close(dbh)
+			del rand, cursor, dbh
 			return found, []
 
-		data = self.__fetchall(cursor, rand)
+		cursor, data = self.__fetchall(cursor, rand)
 		cursor.close()
+		self.__db_close(dbh)
 
-		del rand, cursor
+		del rand, cursor, dbh
 		return found, data
 
 
+	def insert_id(self, table_name, value_dict):
+		""" Simple INSERT """
+		return self.__insert('INSERT', table_name, value_dict, 1)
+
 	def insert(self, table_name, value_dict):
 		""" Simple INSERT """
-		return self.__insert('INSERT', table_name, value_dict)
+		return self.__insert('INSERT', table_name, value_dict, 0)
 
 	def replace(self, table_name, value_dict):
 		""" Simple REPLACE """
-		return self.__insert('REPLACE', table_name, value_dict)
+		return self.__insert('REPLACE', table_name, value_dict, 0)
 
-	def __insert(self, sql_type, table_name, value_dict):
+	def __insert(self, sql_type, table_name, value_dict, insertid=0):
 		""" Simple INSERT / REPLACE """
 
 		if not table_name or not value_dict:
@@ -299,13 +326,21 @@ class Connect(object):
 			return 0
 
 		(sql_set, sql_param) = _sql_set(value_dict)
-
-		cursor = self.__dbh.cursor(MySQLdb.cursors.DictCursor)
-		found = self.__execute(cursor, '%s INTO `%s` SET %s' % (\
+		
+		dbh = self.__dbi()
+		cursor = dbh.cursor()
+		cursor, found = self.__execute(cursor, '%s INTO `%s` SET %s' % (\
 			sql_type, table_name, sql_set),\
 			sql_param)
 		cursor.close()
 
+		# insert id
+		if insertid:
+			found = dbh.insert_id()
+
+		self.__db_close(dbh)
+
+		del cursor
 		return found
 
 
@@ -322,12 +357,15 @@ class Connect(object):
 		for param_name in params2:
 			sql_param.append(param_name)
 
-		cursor = self.__dbh.cursor(MySQLdb.cursors.DictCursor)
-		found = self.__execute(cursor, 'UPDATE `%s` SET %s %s %s' % (\
+		dbh = self.__dbi()
+		cursor = dbh.cursor()
+		cursor, found = self.__execute(cursor, 'UPDATE `%s` SET %s %s %s' % (\
 			table_name, sql_set, sql_where, _sql_limit(limit)),\
 			sql_param)
 		cursor.close()
+		self.__db_close(dbh)
 
+		del cursor
 		return found
 
 
@@ -343,12 +381,15 @@ class Connect(object):
 
 		(sql_where, sql_param) = _sql_where(where_dict)
 
-		cursor = self.__dbh.cursor(MySQLdb.cursors.DictCursor)
-		found = self.__execute(cursor, 'DELETE FROM `%s` %s %s' % (\
+		dbh = self.__dbi()
+		cursor = dbh.cursor()
+		cursor, found = self.__execute(cursor, 'DELETE FROM `%s` %s %s' % (\
 			table_name, sql_where, _sql_limit(limit)),\
 			sql_param)
 		cursor.close()
+		self.__db_close(dbh)
 
+		del cursor
 		return found
 
 
--- test/example/basic_example.py
+++ test/example/basic_example.py
@@ -50,6 +50,10 @@ print(DBH.delete('test_table', {'value_str':'new', 'value_int':100}, limit=1))
 DBH.commit()
 print()
 
+print('reconnect:')
+DBH.close()
+DBH.connect()
+print()
 
 print('cursor execute: select')
 for ni in range(1, 3):
@@ -57,11 +61,7 @@ for ni in range(1, 3):
 	print(DBH.query('SELECT * FROM test_table LIMIT 2'))
 print()
 
-print('reconnect:')
-DBH.close()
-DBH.connect()
-print()
-
+DBH = mysqlwrapper.Connect(user='test_user', passwd='test_passwd', db='test_db', param={'debug':1, 'separate_connect':1})
 print('cursor execute: insert')
 for no in range(100, 104):
 	pprint.pprint(DBH.insert('test_table', {'value_str':'new%s' %no, 'value_int':no}))
